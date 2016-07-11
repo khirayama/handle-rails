e125a8cc72118df14287cb9b4ad644a1a9c8f5be
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-07-11 19:58:02 +0900

    Create task category

--- Gemfile
+++ Gemfile
@@ -17,7 +17,10 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
-  gem 'byebug', platform: :mri
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-byebug'
+  gem 'pry-stack_explorer'
 end
 
 group :development do
--- app/controllers/task_categories_controller.rb
+++ app/controllers/task_categories_controller.rb
@@ -1,8 +1,50 @@
 class TaskCategoriesController < ApplicationController
+  before_action :authenticate
+
   def index
     @task_categories = current_user.created_task_categories
   end
 
-  def create
+  def new
+    @task_category = current_user.created_task_categories.build
   end
+
+  def create
+    @task_category = current_user.created_task_categories.build(task_category_params)
+    @task_category.order = current_user.created_task_categories.length
+    if @task_category.save
+      redirect_to @task_category
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task_category = TaskCategory.find(params[:id])
+  end
+
+  def edit
+    @task_category = current_user.created_task_categories.find(params[:id])
+  end
+
+  def update
+    @task_category = current_user.created_task_categories.find(params[:id])
+    if @task_category.update(task_category_params)
+      redirect_to @task_category
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task_category = current_user.created_task_categories.find(params[:id])
+    @task_category.destroy!
+    redirect_to task_categories_path
+  end
+
+  private
+
+    def task_category_params
+      params.require(:task_category).permit(:name)
+    end
 end
