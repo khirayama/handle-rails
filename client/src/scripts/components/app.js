1d605180da75d6e899427c758ba92c1209a42ffc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-07-31 16:32:49 +0900

    Create isMobileUI function

--- client/src/scripts/components/app.js
+++ client/src/scripts/components/app.js
@@ -10,6 +10,50 @@ import TaskCategoriesPage from '../components/task-categories-page';
 import SettingsPage from '../components/settings-page';
 import HelpPage from '../components/help-page';
 
+const isMobileUI = () => {
+  const ua = window.navigator.userAgent;
+
+  let isTouchable = false;
+  let osName = null;
+
+  // get is touchable
+  if (window.ontouchstart === null) {
+    isTouchable = true;
+  }
+
+  // get os name by user agent
+  switch (true) {
+    case (ua.indexOf('Macintosh') !== -1):
+      osName = 'mac';
+      break;
+    case (ua.indexOf('Windows') !== -1):
+      osName = 'win';
+      break;
+    case (ua.indexOf('Android') !== -1):
+      osName = 'android';
+      break;
+    case (ua.indexOf('iPhone') !== -1):
+      osName = 'ios';
+      break;
+      defalut:
+        osName = 'unknown';
+        break;
+  }
+
+  // judgement ui
+  let isMobileUI = false;
+
+  switch (true) {
+    case (isTouchable && osName == 'android'):
+      isMobileUI = true;
+      break;
+    case (isTouchable && osName == 'ios'):
+      isMobileUI = true;
+      break;
+  }
+  return isMobileUI;
+};
+
 
 const propTypes = {
   appStore: React.PropTypes.object.isRequired,
